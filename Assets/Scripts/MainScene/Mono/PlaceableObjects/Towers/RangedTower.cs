21ac8dd83e986864073171061b7c6e54f249fc42
Nathan Bagge <natbag98@kingshigh.school.nz>
2025-02-18 20:05:43 +1300

    Set towers to fire projectiles at enemies

--- Assets/Scripts/MainScene/Mono/PlaceableObjects/Towers/RangedTower.cs
+++ Assets/Scripts/MainScene/Mono/PlaceableObjects/Towers/RangedTower.cs
@@ -2,6 +2,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RangedTower : Tower {
+    [Header("Ranged Tower")]
+    [SerializeField] private GameObject projectileToShoot;
+    [SerializeField] private Transform shootPoint;
+
     protected override void GetTarget() {
         List<Character> characters_in_range = new();
         foreach (Plot plot in GetPlotsInRange()) characters_in_range.AddRange(plot.GetCharacters());
@@ -9,6 +13,14 @@ public class RangedTower : Tower {
     }
 
     protected override void Attack() {
-        Debug.Log(target);
+        Projectile projectile = Instantiate(
+            projectileToShoot,
+            shootPoint.position,
+            Quaternion.identity,
+            RunManager.instance.projectileContainer
+        ).GetComponent<Projectile>();
+        projectile.SetDamage(attributes.GetAttribute(GameManager.Attributes.Attack));
+        projectile.SetTarget(target);
+        projectile.Setup();
     }
 }
