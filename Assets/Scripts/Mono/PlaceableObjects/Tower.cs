6dec1349bfd4e3e56a239e687732a10e3e1d0772
Nathan Bagge <natbag98@kingshigh.school.nz>
2025-02-20 22:26:06 +1300

    Added health and reload ui towers
    Spawner towers may need fixing/reworking at some point

--- Assets/Scripts/Mono/PlaceableObjects/Tower.cs
+++ Assets/Scripts/Mono/PlaceableObjects/Tower.cs
@@ -2,14 +2,19 @@ using System.Collections;
 using UnityEngine;
 
 public abstract class Tower : PlaceableObject {
+    [Header("Tower : References")]
+    [SerializeField] private UnityEngine.UI.Image reloadBar;
+
     protected Transform target;
     protected bool canAttack = true;
+    protected float reloadTimer;
 
     protected virtual void GetTarget() {}
     protected virtual void Attack() {}
 
     protected IEnumerator Reload() {
         canAttack = false;
+        reloadTimer = 0;
         yield return new WaitForSeconds(attributes.GetAttribute(GameManager.Attributes.ReloadTime));
         canAttack = true;
     }
@@ -25,7 +30,18 @@ public abstract class Tower : PlaceableObject {
         }
     }
 
+    protected override void UpdateUI() {
+        base.UpdateUI();
+        reloadBar.fillAmount = reloadTimer / attributes.GetAttribute(GameManager.Attributes.ReloadTime);
+    }
+
+    protected override void Start() {
+        base.Start();
+        reloadTimer = attributes.GetAttribute(GameManager.Attributes.ReloadTime);
+    }
+
     protected override void Update() {
+        reloadTimer += Time.deltaTime;
         base.Update();
         UpdateAttack();
     }
