458a6cc1e4ec3264f1c70e62428fc9a2e1b4e630
Nathan Bagge <natbag98@kingshigh.school.nz>
2025-02-24 14:14:33 +1300

    Fixed a potential null error in faction creation

--- Assets/Scripts/Classes/Faction.cs
+++ Assets/Scripts/Classes/Faction.cs
@@ -13,8 +13,8 @@ public class Faction {
         string name=null,
         string ruler=null
     ) {
-        if (name.Length == 0) name = null;
-        if (ruler.Length == 0) ruler = null;
+        if (name?.Length == 0) name = null;
+        if (ruler?.Length == 0) ruler = null;
         
         if (faction_type != null) {
             FactionType = (GameManager.FactionTypes)faction_type;
