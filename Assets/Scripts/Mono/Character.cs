747a847a8d08208b054dcd5e4a2db8a5aa21e9d7
Nathan Bagge <natbag98@kingshigh.school.nz>
2025-02-28 11:43:28 +1300

    Fixed a bug in character movement

--- Assets/Scripts/Mono/Character.cs
+++ Assets/Scripts/Mono/Character.cs
@@ -136,7 +136,8 @@ public abstract class Character : MonoBehaviour, IRangedTarget {
         lastPosition = transform.position;
     }
 
-    public void SetStartPos(Vector3 position) { // TODO : Stop this from causing collision issues due to being moved after instantiation
+    public void SetStartPos(Vector3 position) { 
+        lastPosition = position;
         transform.position = new Vector3(
             position.x - moveOffset.x,
             0f,
