87c84ea0f4bc6bd193dcf03166a38ec1cbd834c4
Nathan Bagge <natbag98@kingshigh.school.nz>
2025-03-16 20:28:20 +1300

    Added buttons to the game scene

--- Assets/Scripts/Mono/Managers/UI/GameSceneUIManager.cs
+++ Assets/Scripts/Mono/Managers/UI/GameSceneUIManager.cs
@@ -2,11 +2,29 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameSceneUIManager : MonoBehaviour {
+    [Header("References")]
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject characterPerkMenu;
+
     public void _Button_StartRunButtonClicked() {
         SceneManager.LoadScene("MainScene");
     }
 
     public void _Button_SaveAndQuitButtonClicked() {
         SceneManager.LoadScene("MainMenuScene");
+    }
+
+    public void _Button_CharacterPerkButtonClicked() {
+        mainMenu.SetActive(false);
+        characterPerkMenu.SetActive(true);
+    }
+
+    public void _Button_ViewTowersButtonClicked() {
+        Debug.Log("View Towers");
+    }
+
+    public void _Button_CharacterPerksBackButtonClicked() {
+        characterPerkMenu.SetActive(false);
+        mainMenu.SetActive(true);
     }
 }
