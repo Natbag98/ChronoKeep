b61cb4aeb619e3d278b6de25be14f766c7883c52
Nathan Bagge <natbag98@kingshigh.school.nz>
2025-03-19 13:01:52 +1300

    Added an event for randomly declaring wars

---
+++ Assets/Scripts/SO/Events/RandomWar.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RandomWar", menuName = "Event/RandomWar")]
+public class RandomWar : SOEvent {
+    [Header("Random War")]
+    [SerializeField] private bool toPlayer;
+
+    private Faction from;
+    private Faction to;
+
+    public override string GetDescription() {
+        return $"{from.Name} has declared war on {to.Name}!";
+    }
+
+    public override void Setup() {
+        from = Utils.Choice(
+            (
+                from faction in GameManager.instance.Game.BaseFactions 
+                where faction.FactionType == GameManager.FactionTypes.Kingdom
+                select faction
+            ).ToArray()
+        );
+
+        if (toPlayer) {
+            to = GameManager.instance.Game.PlayerFaction;
+        } else {
+            List<Faction> valid_to = (
+                from faction in GameManager.instance.Game.BaseFactions 
+                where faction.FactionType == GameManager.FactionTypes.Kingdom
+                select faction
+            ).ToList();
+            valid_to.Remove(from);
+            valid_to.Add(GameManager.instance.Game.PlayerFaction);
+            to = Utils.Choice(valid_to);
+        }
+    }
+
+    public override void Event() {
+        from.DeclareWar(to);
+    }
+}
