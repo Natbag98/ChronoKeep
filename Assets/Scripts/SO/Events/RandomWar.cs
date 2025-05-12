7e176cd7753e387d0a36a3adc8fec60cb91f00a6
Nathan Bagge <natbag98@kingshigh.school.nz>
2025-05-12 14:19:26 +1200

    Added a new random war event

--- Assets/Scripts/SO/Events/RandomWar.cs
+++ Assets/Scripts/SO/Events/RandomWar.cs
@@ -15,29 +15,24 @@ public class RandomWar : SOEvent {
     }
 
     public override void Setup() {
-        while (true) {
-            from = Utils.Choice(
-                (
-                    from faction in GameManager.instance.Game.BaseFactions 
-                    where faction.FactionType == GameManager.FactionTypes.Kingdom
-                    select faction
-                ).ToArray()
-            );
+        from = Utils.Choice(
+            (
+                from faction in GameManager.instance.Game.BaseFactions 
+                where faction.FactionType == GameManager.FactionTypes.Kingdom
+                select faction
+            ).ToArray()
+        );
 
-            if (toPlayer) {
-                to = GameManager.instance.Game.PlayerFaction;
-            } else {
-                List<Faction> valid_to = (
-                    from faction in GameManager.instance.Game.BaseFactions 
-                    where faction.FactionType == GameManager.FactionTypes.Kingdom
-                    select faction
-                ).ToList();
-                valid_to.Remove(from);
-                valid_to.Add(GameManager.instance.Game.PlayerFaction);
-                to = Utils.Choice(valid_to);
-            }
-
-            if (from != to) { return; }
+        if (toPlayer) {
+            to = GameManager.instance.Game.PlayerFaction;
+        } else {
+            List<Faction> valid_to = (
+                from faction in GameManager.instance.Game.BaseFactions 
+                where faction.FactionType == GameManager.FactionTypes.Kingdom
+                select faction
+            ).ToList();
+            valid_to.Remove(from);
+            to = Utils.Choice(valid_to);
         }
     }
 
