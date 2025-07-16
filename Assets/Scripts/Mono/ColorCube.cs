28856dc1b7ad6c41870959c4b6cf5d9a2be8b296
Nathan Bagge <natbag98@kingshigh.school.nz>
2025-07-16 13:26:10 +1200

    Started working on faction colors

---
+++ Assets/Scripts/Mono/ColorCube.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ColorCube : MonoBehaviour {
+    [SerializeField] private Plot plot;
+    [SerializeField] private MeshRenderer mesh;
+
+    void Start() {
+        mesh.material.color = new Color((float)GameManager.Random.NextDouble(), mesh.material.color.g, mesh.material.color.b, (float)GameManager.Random.NextDouble());
+    }
+
+    void Update() {
+        if (plot.faction == null) {
+            mesh.material.color = new(mesh.material.r, mesh.material, mesh.material,)
+        }
+    }
+}
