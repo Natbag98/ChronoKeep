7b0ebb822fa8c7c68d744bbd8e63e41fa92d4c31
Nathan Bagge <natbag98@kingshigh.school.nz>
2025-08-04 19:37:39 +1200

    Added character faction indicator

--- Assets/Scripts/Mono/UI/EnemyDot.cs
+++ Assets/Scripts/Mono/UI/EnemyDot.cs
@@ -1,19 +1,29 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyDot : MonoBehaviour {
-    public float height;
+    [SerializeField] private Image dot;
+    [SerializeField] private float height;
     
     [HideInInspector] public GameObject character;
 
-    private void Update() {
-        if (character.gameObject == null) {
-            Destroy(gameObject);
+    private void Start() {
+        if (character.GetComponent<Character>().faction == GameManager.instance.Game.PlayerFaction) {
+            dot.color = new(0, 0, 255);
+        } else {
+            dot.color = new(255, 0, 0);
         }
+    }
 
-        transform.position = new(
-            character.transform.position.x,
-            height,
-            character.transform.position.z
-        );
+    private void Update() {
+        if (character == null) {
+            Destroy(gameObject);
+        } else {
+            transform.position = new(
+                character.transform.position.x,
+                height,
+                character.transform.position.z
+            );
+        }
     }
 }
